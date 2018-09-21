0dc2f22daa7ad4117890778adccb143789a8bc92
xomrau <hle3524@gmail.com>
2018-09-21 17:23:05 +1200

    added matchups list

---
+++ classes/matchups.js
@@ -0,0 +1,575 @@
+exports.Matchups = function() {
+    var matchup = this;
+    
+    matchup.listOfChamps = {
+        'ahri': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+            
+        },
+
+        'akali': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'anivia': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'annie': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+        
+        },
+
+        'aurelion': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+        'aurelionsol': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+        'asol': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'azir': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'brand': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'cass': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+        'cassiopeia': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'corki': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'diana': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'ekko': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'ezreal': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'fiddle': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+        'fiddlesticks': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'fizz': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'galio': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'gangplank': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+        'gp': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'heimer': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+        'heimerdinger': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'jayce': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'karma': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'karthus': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'kass': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+        'kassadin': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'kat': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+        'katarina': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'leblanc': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'lissandra': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'lucian': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'lux': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'malzahar': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'morgana': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'ori': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+        'orianna': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'pantheon': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'ryze': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'swain': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'syndra': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'taliyah': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'talon': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'tf': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+        'twistedfate': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'veigar': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'velkoz':{
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+        'vel\'koz': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'viktor': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'vladimir': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'xerath': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'yasuo': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'zed': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'ziggs': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'zilean': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'zoe': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+        'zyra': {
+            difficult: '',
+            win_rate: '',
+            starting_items: '',
+            rune: '',
+            sum_spells: '',
+            brief: '',
+
+        },
+
+    }
+}
