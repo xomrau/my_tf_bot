0e0ac0abe911abf9274af0c016ade004c4b9b2b5
xomrau <hle3524@gmail.com>
2018-09-28 22:48:25 +1200

    edited

--- classes/helps.js
+++ classes/helps.js
@@ -3,51 +3,51 @@ exports.Helps = function() {
 
     commands.listOfReponses = {
         'help': {
-            description: 'returns help contents.',
-            title: '',
+            description: `returns help contents.`,
+            title: ``,
 
         },
 
         'build': {
-            description: 'returns recommended twisted fate\'s build.',
-            title: '',
+            description: `returns recommended twisted fate\'s build.`,
+            title: ``,
 
         },
 
         'patch': {
-            description: 'returns the latest patch.',
-            title: '',
+            description: `returns the latest patch.`,
+            title: ``,
         },
 
         'matchup': {
             // winrate, 
-            description: 'syntax: !matchup [champion\'s name] - returns a brief information of the matchup',
-            title: '',
+            description: `syntax: !matchup [champion\'s name] - returns a brief information of the matchup`,
+            title: ``,
         },
 
         'runes': {
-            description: 'returns the current runepage of the latest patch.',
-            title: '',
+            description: `returns the current runepage of the latest patch.`,
+            title: ``,
         },
 
         'clubs': {
-            description: '',
-            title: '',
+            description: ``,
+            title: ``,
         },
 
         'dopa': {
-            description: 'returns dopa\'s op.gg, stream, youtube channel.',
-            title: 'Dopa/Apdo, God\'s Hands.',
+            description: `returns dopa\'s op.gg, stream, youtube channel.`,
+            title: `Dopa/Apdo, God\'s Hands.`,
         },
 
         'grossgore': {
-            description: 'returns grossgore\'s op.gg, stream, youtube channel.',
-            title: 'Gross Gore, self-proclaimed God.',
+            description: `returns grossgore\'s op.gg, stream, youtube channel.`,
+            title: `Gross Gore, self-proclaimed God.`,
         },
 
         'hax': {
-            description: 'return hax$\'s op.gg',
-            title: 'SSBM Hax, Challenger Twisted Fate Main.',
+            description: `return hax$\'s op.gg`,
+            title: `SSBM Hax, Challenger Twisted Fate Main.`,
         },
     }
 
--- tf_bot.js
+++ tf_bot.js
@@ -1,15 +1,16 @@
 // to include a module, use require() fn w/ the name of the module
 //
 const Discord = require('discord.js');
+const Matchup = require('./classes/matchups.js');
 
 // starting point for any bots: new Discord.Client(options)
-const client = new Discord.Client()
-const key = require('./key.json')
+const client = new Discord.Client();
+const key = require('./key.json');
 
 client.on('ready', () => {
     console.log(client.readyAt) // time at which the client was last regarded as being in a READY state
     console.log('Connected as ' + client.user.tag) // .tag = discord tag for the users ie. TwistedFate Bot#2013
-})
+});
 
 client.on('ready', () => {
     // List servers the bot is connected to
@@ -17,7 +18,7 @@ client.on('ready', () => {
     client.guilds.forEach((guild) => {
         console.log(' - ' + guild.name)
     })
-}) 
+});
 
 
 // COMMANDS
@@ -29,36 +30,58 @@ client.on('ready', () => {
 client.on('message', (messageReceived) => {
     if (messageReceived.author == client.user) {
         return // Prevent bot from responding to its own msgs
-    }
+    };
 
     if (messageReceived.content.startsWith('!')){
         // js str.startsWith(' ')
         // .content = content of the message
         processCommand(messageReceived)
-    }
-}) 
+    };
+}) ;
 
 function processCommand(messageReceived) { // breakdown the command
-    let fullCommand = messageReceived.content.substr(1) // remove '!'
-    let splitCommand = fullCommand.split(' ') // split the full command 
-    let primCommand = splitCommand[0] // first word is the primary command
+    let fullCommand = messageReceived.content.substr(1); // remove '!'
+    let splitCommand = fullCommand.split(' '); // split the full command into a list 
+    let primCommand = splitCommand[0]; // first word is the primary command
     // array.slice(start, end)
-    let arg = splitCommand.slice(1) // other words = args/params for the command
+    
+    let arg = splitCommand.slice(1); // other words = args/params for the command
+    
+    
 
-    console.log('Command: ' + primCommand)
-    console.log('Argument(s): ' + arg)
+    console.log('Command: ' + primCommand);
+    console.log('Argument(s): ' + arg);
 
     if (primCommand == 'help') {
-        helpCommand(arg, messageReceived)
-    } else if (a) {
+        helpCommand(arg, messageReceived);
+    } else if (primCommand == 'matchup') {
+        if (arg.length == 0){
+            messageReceived.channel.send('Hey partner, can you be more specific by writing `!matchup [champion\'s name]`.')
+        } else{
+            matchupCommand(arg, messageReceived)
+        }
 
     } else {
         messageReceived.channel.send('Hold it partner, I reckon you try `!help`.')
     }
-}
+};
 
 function helpCommand(arg, messageReceived) {
 
-}
+};
 
-client.login(key.discord_bot_secret_token)
+function matchupCommand(arg, messageReceived){
+    const name = 'matchup';
+    let matchup = new Matchup.Matchups();
+    let champName = arg.toString(); // This is only temporary. Will make a seperate function that format inputs
+    for (i in matchup.listOfChamps){
+        if (champName === i){
+            champName = champName.charAt(0).toUpperCase() + champName.substr(1)
+            messageReceived.channel.send(`'**Twisted Fate vs ${champName}**'`);
+            
+            console.log('Respond to command: ${name} ${arg}');
+        }
+    };
+};
+
+client.login(key.discord_bot_secret_token);
